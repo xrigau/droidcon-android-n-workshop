07e937bb3e79ac7b1ad3aa93ee94c962af3460ab
Xavi Rigau <xavi@novoda.com>
2016-03-26 15:30:07 +0400

    Adds multiwindow drag and drop support

--- app/src/main/java/me/oabbasi/droidcon/androidn/NoteActivity.java
+++ app/src/main/java/me/oabbasi/droidcon/androidn/NoteActivity.java
@@ -14,7 +14,7 @@ import android.view.View;
 import android.widget.GridView;
 import android.widget.Toast;
 
-public class NoteActivity extends AppCompatActivity /*implements View.OnDragListener*/ {
+public class NoteActivity extends AppCompatActivity implements View.OnDragListener {
 
     GridView gridView;
 
@@ -47,7 +47,7 @@ public class NoteActivity extends AppCompatActivity /*implements View.OnDragList
 
         gridView = (GridView) findViewById(R.id.notesGridView);
         gridView.setAdapter(new NoteItemsAdapter(this, notes));
-//        gridView.setOnDragListener(this);
+        gridView.setOnDragListener(this);
     }
 
     @Override
@@ -73,110 +73,110 @@ public class NoteActivity extends AppCompatActivity /*implements View.OnDragList
         return super.onOptionsItemSelected(item);
     }
 
-//    // This is the method that the system calls when it dispatches a drag event to the
-//    // listener.
-//    public boolean onDrag(View v, DragEvent event) {
-//
-//        // Defines a variable to store the action type for the incoming event
-//        final int action = event.getAction();
-//
-//        // Handles each of the expected events
-//        switch (action) {
-//
-//            case DragEvent.ACTION_DRAG_STARTED:
-//
-//                // Determines if this View can accept the dragged data
-//                if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
-//
-//                    // As an example of what your application might do,
-//                    // applies a blue color tint to the View to indicate that it can accept
-//                    // data.
-//                    v.setBackgroundColor(Color.BLUE);
-//
-//                    // Invalidate the view to force a redraw in the new tint
-//                    v.invalidate();
-//
-//                    // returns true to indicate that the View can accept the dragged data.
-//                    return true;
-//
-//                }
-//
-//                // Returns false. During the current drag and drop operation, this View will
-//                // not receive events again until ACTION_DRAG_ENDED is sent.
-//                return false;
-//
-//            case DragEvent.ACTION_DRAG_ENTERED:
-//
-//                // Applies a green tint to the View. Return true; the return value is ignored.
-//
-//                v.setBackgroundColor(Color.GREEN);
-//
-//                // Invalidate the view to force a redraw in the new tint
-//                v.invalidate();
-//
-//                return true;
-//
-//            case DragEvent.ACTION_DRAG_LOCATION:
-//
-//                // Ignore the event
-//                return true;
-//
-//            case DragEvent.ACTION_DRAG_EXITED:
-//
-//                // Re-sets the color tint to blue. Returns true; the return value is ignored.
-//                v.setBackgroundColor(Color.BLUE);
-//
-//                // Invalidate the view to force a redraw in the new tint
-//                v.invalidate();
-//
-//                return true;
-//
-//            case DragEvent.ACTION_DROP:
-//
-//                // Gets the item containing the dragged data
-//                ClipData.Item item = event.getClipData().getItemAt(0);
-//
-//                // Gets the text data from the item.
-//                String dragData = item.getText().toString();
-//
-//                // Displays a message containing the dragged data.
-//                Toast.makeText(this, "Dragged data is " + dragData, Toast.LENGTH_LONG).show();
-//
-//                // Turns off any color tints
-//                v.setBackgroundColor(Color.TRANSPARENT);
-//
-//                // Invalidates the view to force a redraw
-//                v.invalidate();
-//
-//                // Returns true. DragEvent.getResult() will return true.
-//                return true;
-//
-//            case DragEvent.ACTION_DRAG_ENDED:
-//
-//                // Turns off any color tinting
-//                v.setBackgroundColor(Color.TRANSPARENT);
-//
-//                // Invalidates the view to force a redraw
-//                v.invalidate();
-//
-//                // Does a getResult(), and displays what happened.
-//                if (event.getResult()) {
-//                    Toast.makeText(this, "The drop was handled.", Toast.LENGTH_LONG).show();
-//
-//                } else {
-//                    Toast.makeText(this, "The drop didn't work.", Toast.LENGTH_LONG).show();
-//
-//                }
-//
-//                // returns true; the value is ignored.
-//                return true;
-//
-//            // An unknown action type was received.
-//            default:
-//                Log.e("DragDrop Example", "Unknown action type received by OnDragListener.");
-//                break;
-//        }
-//
-//        return false;
-//    }
+    // This is the method that the system calls when it dispatches a drag event to the
+    // listener.
+    public boolean onDrag(View v, DragEvent event) {
+
+        // Defines a variable to store the action type for the incoming event
+        final int action = event.getAction();
+
+        // Handles each of the expected events
+        switch (action) {
+
+            case DragEvent.ACTION_DRAG_STARTED:
+
+                // Determines if this View can accept the dragged data
+                if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+
+                    // As an example of what your application might do,
+                    // applies a blue color tint to the View to indicate that it can accept
+                    // data.
+                    v.setBackgroundColor(Color.BLUE);
+
+                    // Invalidate the view to force a redraw in the new tint
+                    v.invalidate();
+
+                    // returns true to indicate that the View can accept the dragged data.
+                    return true;
+
+                }
+
+                // Returns false. During the current drag and drop operation, this View will
+                // not receive events again until ACTION_DRAG_ENDED is sent.
+                return false;
+
+            case DragEvent.ACTION_DRAG_ENTERED:
+
+                // Applies a green tint to the View. Return true; the return value is ignored.
+
+                v.setBackgroundColor(Color.GREEN);
+
+                // Invalidate the view to force a redraw in the new tint
+                v.invalidate();
+
+                return true;
+
+            case DragEvent.ACTION_DRAG_LOCATION:
+
+                // Ignore the event
+                return true;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+
+                // Re-sets the color tint to blue. Returns true; the return value is ignored.
+                v.setBackgroundColor(Color.BLUE);
+
+                // Invalidate the view to force a redraw in the new tint
+                v.invalidate();
+
+                return true;
+
+            case DragEvent.ACTION_DROP:
+
+                // Gets the item containing the dragged data
+                ClipData.Item item = event.getClipData().getItemAt(0);
+
+                // Gets the text data from the item.
+                String dragData = item.getText().toString();
+
+                // Displays a message containing the dragged data.
+                Toast.makeText(this, "Dragged data is " + dragData, Toast.LENGTH_LONG).show();
+
+                // Turns off any color tints
+                v.setBackgroundColor(Color.TRANSPARENT);
+
+                // Invalidates the view to force a redraw
+                v.invalidate();
+
+                // Returns true. DragEvent.getResult() will return true.
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+
+                // Turns off any color tinting
+                v.setBackgroundColor(Color.TRANSPARENT);
+
+                // Invalidates the view to force a redraw
+                v.invalidate();
+
+                // Does a getResult(), and displays what happened.
+                if (event.getResult()) {
+                    Toast.makeText(this, "The drop was handled.", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(this, "The drop didn't work.", Toast.LENGTH_LONG).show();
+
+                }
+
+                // returns true; the value is ignored.
+                return true;
+
+            // An unknown action type was received.
+            default:
+                Log.e("DragDrop Example", "Unknown action type received by OnDragListener.");
+                break;
+        }
+
+        return false;
+    }
 }
--- app/src/main/java/me/oabbasi/droidcon/androidn/NoteItemsAdapter.java
+++ app/src/main/java/me/oabbasi/droidcon/androidn/NoteItemsAdapter.java
@@ -13,8 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -54,29 +52,29 @@ public class NoteItemsAdapter extends BaseAdapter {
                 .findViewById(R.id.noteItemCheck);
         noteItemCheckBox.setChecked(position % 3 == 0);
 
-//        noteItem.setOnLongClickListener(new View.OnLongClickListener() {
-//            @Override
-//            public boolean onLongClick(View v) {
-//                ClipData.Item item = new ClipData.Item(v.getTag().toString());
-//
-//                // Create a new ClipData using the note as a label, the plain text MIME type, and
-//                // the already-created item. This will create a new ClipDescription object within the
-//                // ClipData, and set its MIME type entry to "text/plain"
-//                ClipData dragData = new ClipData(v.getTag().toString(), new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN},item);
-//
-//                // Instantiates the drag shadow builder.
-//                View.DragShadowBuilder myShadow = new MyDragShadowBuilder(v);
-//
-//                // Starts the drag
-//
-//                v.startDragAndDrop(dragData,  // the data to be dragged
-//                        myShadow,  // the drag shadow builder
-//                        null,      // no need to use local data
-//                        View.DRAG_FLAG_GLOBAL
-//                );
-//                return true;
-//            }
-//        });
+        noteItem.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ClipData.Item item = new ClipData.Item(v.getTag().toString());
+
+                // Create a new ClipData using the note as a label, the plain text MIME type, and
+                // the already-created item. This will create a new ClipDescription object within the
+                // ClipData, and set its MIME type entry to "text/plain"
+                ClipData dragData = new ClipData(v.getTag().toString(), new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN}, item);
+
+                // Instantiates the drag shadow builder.
+                View.DragShadowBuilder myShadow = new MyDragShadowBuilder(v);
+
+                // Starts the drag
+
+                v.startDragAndDrop(dragData,  // the data to be dragged
+                        myShadow,  // the drag shadow builder
+                        null,      // no need to use local data
+                        View.DRAG_FLAG_GLOBAL
+                );
+                return true;
+            }
+        });
 
         return noteItem;
     }
@@ -96,55 +94,55 @@ public class NoteItemsAdapter extends BaseAdapter {
         return 0;
     }
 
-//    private static class MyDragShadowBuilder extends View.DragShadowBuilder {
-//
-//        // The drag shadow image, defined as a drawable thing
-//        private static Drawable shadow;
-//
-//        // Defines the constructor for myDragShadowBuilder
-//        public MyDragShadowBuilder(View v) {
-//
-//            // Stores the View parameter passed to myDragShadowBuilder.
-//            super(v);
-//
-//            // Creates a draggable image that will fill the Canvas provided by the system.
-//            shadow = new ColorDrawable(Color.LTGRAY);
-//        }
-//
-//        // Defines a callback that sends the drag shadow dimensions and touch point back to the
-//        // system.
-//        @Override
-//        public void onProvideShadowMetrics (Point size, Point touch) {
-//            // Defines local variables
-//            int width, height;
-//
-//            // Sets the width of the shadow to half the width of the original View
-//            width = getView().getWidth() / 2;
-//
-//            // Sets the height of the shadow to half the height of the original View
-//            height = getView().getHeight() / 2;
-//
-//            // The drag shadow is a ColorDrawable. This sets its dimensions to be the same as the
-//            // Canvas that the system will provide. As a result, the drag shadow will fill the
-//            // Canvas.
-//            shadow.setBounds(0, 0, width, height);
-//
-//            // Sets the size parameter's width and height values. These get back to the system
-//            // through the size parameter.
-//            size.set(width, height);
-//
-//            // Sets the touch point's position to be in the middle of the drag shadow
-//            touch.set(width / 2, height / 2);
-//        }
-//
-//        // Defines a callback that draws the drag shadow in a Canvas that the system constructs
-//        // from the dimensions passed in onProvideShadowMetrics().
-//        @Override
-//        public void onDrawShadow(Canvas canvas) {
-//
-//            // Draws the ColorDrawable in the Canvas passed in from the system.
-//            shadow.draw(canvas);
-//        }
-//    }
+    private static class MyDragShadowBuilder extends View.DragShadowBuilder {
+
+        // The drag shadow image, defined as a drawable thing
+        private static Drawable shadow;
+
+        // Defines the constructor for myDragShadowBuilder
+        public MyDragShadowBuilder(View v) {
+
+            // Stores the View parameter passed to myDragShadowBuilder.
+            super(v);
+
+            // Creates a draggable image that will fill the Canvas provided by the system.
+            shadow = new ColorDrawable(Color.LTGRAY);
+        }
+
+        // Defines a callback that sends the drag shadow dimensions and touch point back to the
+        // system.
+        @Override
+        public void onProvideShadowMetrics (Point size, Point touch) {
+            // Defines local variables
+            int width, height;
+
+            // Sets the width of the shadow to half the width of the original View
+            width = getView().getWidth() / 2;
+
+            // Sets the height of the shadow to half the height of the original View
+            height = getView().getHeight() / 2;
+
+            // The drag shadow is a ColorDrawable. This sets its dimensions to be the same as the
+            // Canvas that the system will provide. As a result, the drag shadow will fill the
+            // Canvas.
+            shadow.setBounds(0, 0, width, height);
+
+            // Sets the size parameter's width and height values. These get back to the system
+            // through the size parameter.
+            size.set(width, height);
+
+            // Sets the touch point's position to be in the middle of the drag shadow
+            touch.set(width / 2, height / 2);
+        }
+
+        // Defines a callback that draws the drag shadow in a Canvas that the system constructs
+        // from the dimensions passed in onProvideShadowMetrics().
+        @Override
+        public void onDrawShadow(Canvas canvas) {
+
+            // Draws the ColorDrawable in the Canvas passed in from the system.
+            shadow.draw(canvas);
+        }
+    }
 
 }
